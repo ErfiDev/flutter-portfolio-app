1f12849c0f1e66c59ffecfacfef5badb90608182
Erfan <erfanhanifezade@gmail.com>
2022-03-17 19:35:21 +0330

    using SizedBox widget

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -7,11 +7,17 @@ class Home extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
+          SizedBox(
+            height: 10,
+          ),
           CircleAvatar(
             backgroundImage: AssetImage(
               'assets/user.png',
             ),
             radius: 70,
+          ),
+          SizedBox(
+            height: 10,
           ),
           Text(
             'Hi, I am Erfan',
